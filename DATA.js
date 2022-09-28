9dbb615704024fa8359b5f60725da805371f6a70
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 12:13:43 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "8965667a-a477-4824-aa4e-7cb497b51fb8",
+    "home_feed": [
+        {
+            "id": "dec23db6-b568-4434-96b4-2397f575af57",
+            "artist_name": "different",
+            "home_carousel": [
+                {
+                    "id": "db437233-88e5-4d9b-8284-60ae35289793",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/117.png"
+                },
+                {
+                    "id": "dca919f6-fae8-4d4b-8e44-fdbdbc239dfc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "f9585029-effb-4262-a8e0-d838e033a96d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                },
+                {
+                    "id": "b8098511-c812-499f-8285-b6c5203e5df0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "ce8e6e7d-d6a6-4fe8-aa78-41e1aba82410",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                }
+            ],
+            "art_caption": "theory",
+            "art_description": "Kitchen put grow my move. Rule development fire fine likely factor again. Data year human population leader her."
+        },
+        {
+            "id": "64a58e40-4179-45f8-acaf-3ef946547dc6",
+            "artist_name": "air",
+            "home_carousel": [
+                {
+                    "id": "b8d28090-5112-4444-930c-dfccc4950b96",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "472926db-8651-4f4f-90cf-921b67e14605",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "3e3453af-51f0-4a8f-a2e9-a6b03df71429",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                },
+                {
+                    "id": "f65963a1-0aa5-4016-8c1e-ecc056f01ced",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/37.png"
+                },
+                {
+                    "id": "b1f5b54f-4fa4-4c40-a14b-a9b83525e729",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                }
+            ],
+            "art_caption": "benefit",
+            "art_description": "Spring crime deal family central. First mouth high away people safe local treatment."
+        },
+        {
+            "id": "32c5cf2a-fdc5-413e-b8f5-1ea5c45bdd58",
+            "artist_name": "long",
+            "home_carousel": [
+                {
+                    "id": "728d3270-3676-4eea-8401-8f9ba6049e51",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "ae980f64-2ac2-4c8f-a898-2c9b8f55eac0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "1b99e4f8-abad-4b88-a476-1041dbbdaadc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "7f13802e-6552-4875-9242-28b66d10ff42",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "b3c10b15-833c-4788-8ad6-470be5f64eb1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                }
+            ],
+            "art_caption": "far",
+            "art_description": "Their board result eight upon. Him audience minute write easy those. Hold receive million similar between me. Our mission face happen check growth paper purpose."
+        },
+        {
+            "id": "485e61a5-d908-4966-a71a-674a7eba46e9",
+            "artist_name": "report",
+            "home_carousel": [
+                {
+                    "id": "e59764e0-9223-4c19-9cbb-7ecb71045e47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/39.png"
+                },
+                {
+                    "id": "f4855a6e-0237-4646-91cf-913811466ef7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png"
+                },
+                {
+                    "id": "5c87b382-d508-4a05-9b5f-534a3e2e3d84",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "32fa8489-74fc-431a-8bab-d40a2dd41c06",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                },
+                {
+                    "id": "f0399f7b-e17c-486c-b908-4971df8cd40a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                }
+            ],
+            "art_caption": "improve",
+            "art_description": "Pm window direction church person long even. Fish size standard. Avoid blood system run low high environment."
+        },
+        {
+            "id": "a1ef1957-20f7-481b-a1fb-2192ae144822",
+            "artist_name": "from",
+            "home_carousel": [
+                {
+                    "id": "fa33eed6-e87e-4e3f-a597-1f19c04eb44c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "2f32bbc1-36a6-403a-937a-473a43e9a587",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+                },
+                {
+                    "id": "e9b95705-4083-4059-94b5-dbdb0c40286d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "4fa5975b-a932-4852-bf6b-4e683f2d0b6a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "faac5ab8-795d-4c52-ae68-c9e08342ef83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                }
+            ],
+            "art_caption": "can",
+            "art_description": "None think interview religious mouth mouth. Could majority me or evidence. Off still close who lay top seek."
+        }
+    ]
+}
